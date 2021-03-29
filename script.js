8a2d8bdcaeb1f559ef54415a5d579b00b02e0a25
Ishmael Ben Israel <Ishincodeable@gmail.com>
2021-03-29 13:36:25 -0500

    Added some code to change launchstatus color

--- script.js
+++ script.js
@@ -4,21 +4,20 @@ window.addEventListener("load", () => {
    const form = document.querySelector("form");
 
    form.addEventListener("submit", (event) => {
+      event.preventDefault();
       let pilotnameInput = document.querySelector("input[name = pilotName]").value;
       let copilotnameInput = document.querySelector("input[name =copilotName]").value;
       let fuelLevelInput = document.querySelector("input[name = fuelLevel]").value;
       let cargoMassInput = document.querySelector("input[name = cargoMass]").value;   
          
-   
-
       if (pilotnameInput === "" || copilotnameInput === "" || 
          fuelLevelInput === "" || cargoMassInput === ""){
          alert("All fields are required!");
-         event.preventDefault();
+     //    event.preventDefault();
       } else if ((typeof(pilotnameInput) != "string")  || 
                  typeof(copilotnameInput) != "string") {
             alert("Must be AlphaNumerical Characters")
-            event.preventDefault()
+      //     event.preventDefault()
       }
       
       if (isNaN(fuelLevelInput) || 
@@ -29,9 +28,32 @@ window.addEventListener("load", () => {
          fuelLevelInput = Number(fuelLevelInput);
          cargoMassInput = Number(cargoMassInput);
       }
+   
+   
+      console.log("fuel level:", fuelLevelInput, "|| cargoMass:", cargoMassInput );
+      
+      let NotReadyToLaunch;
+      
+      if (fuelLevelInput < 10000 || cargoMassInput > 10000){
+         NotReadyToLaunch = true;
+      } else {NotReadyToLaunch = false} //We have liftOff- GodSpeed
+      
+      if (NotReadyToLaunch){
 
-      console.log("fuel level:", fuelLevelInput, "|| type: ", typeof(fuelLevelInput) );
-      console.log("pilot's name:", pilotnameInput, "|| type: ", typeof(pilotnameInput) );
+         document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotnameInput} is ready for launch`;
+         document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilotnameInput} is ready for launch`;
+
+         
+         if (fuelLevelInput < 10000) {
+            document.getElementById("faultyItems").style.visibility = "visible";
+            document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch"
+            document.getElementById("launchStatus").style.color = "red"
+            }
+
+      }
+      console.log(document.getElementById("faultyItems").innerHTML)
+      console.log(document.getElementById("pilotStatus").innerHTML)
+      console.log(document.getElementById("copilotStatus").innerHTML)
    });//end of form
 
          
@@ -40,7 +62,35 @@ window.addEventListener("load", () => {
 }); //end of window block  
    
 
+/*
+      let NotReadyToLaunch = true;
 
+      let updateRequirements = (documentByID) => {
+            let shuttleStatus = `
+               <ol>
+                  <li id="pilotStatus">Pilot ${pilotnameInput.value} is ready for launch</li>
+                  <li id="copilotStatus">Co-pilot ${copilotnameInput.value} is ready for launch</li>
+                  `;
+
+            if (Number(fuelLevelInput) < 10000) {
+               documentByID.style.visibility = "visible";
+               shuttleStatus += 
+                  `<li id="fuelStatus">Fuel level too low for launch</li>`;
+               
+               launchStatus = document.getElementById('launchStatus'); 
+               launchStatus.innerHTML = "Shuttle not ready for launch";
+               launchStatus.style.color = "red";
+            }
+            documentByID.innerHTML =  shuttleStatus;
+            return documentByID.innerHTML;
+         }
+      if (NotReadyToLaunch) {
+        
+      }
+      let faultyItems = document.getElementById("faultyItems");
+      console.log(updateRequirements(faultyItems));
+      console.log(faultyItems.innerHTML);
+*/
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
